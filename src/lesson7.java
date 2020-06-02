f839b4aec1cfb5196be3e77a8512d21d80330778
Mikhail Kamenev <m.kamenev89@yandex.ru>
2020-06-02 17:13:54 +0300

    new version

---
+++ src/lesson7.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;  // автоматически подключается при написании класса
+
+public class lesson7 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in); // System.in это стандартный поток входных данных
+                                            // Scanner - это класс
+                                            // С помощью слова new мы создаем новый объект класса Scanner
+    }
+}
